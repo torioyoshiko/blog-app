8a56b1b894f351a9fe49bd7daff206d8eafd0d7c
Torio Yoshiko <torio.yoshiko@gmail.com>
2018-07-16 18:04:11 +0300

    four
    almost works

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,10 +1,13 @@
-const postReducer = (state = [], action) => {
+function postReducer (state = [], action) {
 
     if (action.type === 'ADD_POST') {
         return state.concat([action.data]);
+    } else if (action.type === 'FETCH_POSTS'){
+        return action.payload;
     } else {
         return state;
     }
+
 };
 
 export default postReducer;
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,4 +1,11 @@
+var loadPost = [
+    {
+        id: 1,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam atque et excepturi exercitationem explicabo hic, omnis repellat soluta ullam voluptate!'
+    }
+];
+
 export const getPosts = () => dispatch => {
         console.log('posts loaded');
-        dispatch({ type: 'FETCH_POSTS', payload: [] });
+        dispatch({ type: 'FETCH_POSTS', payload: loadPost});
 };
