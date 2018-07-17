2d254311d6af77981654f25cecbf3ea0cd26c929
Torio Yoshiko <torio.yoshiko@gmail.com>
2018-07-17 10:12:54 +0300

    four

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -11,3 +11,29 @@ function postReducer (state = [], action) {
 };
 
 export default postReducer;
+
+/*
+function postReducer(store = {}, {payload, type}) {
+    switch (type) {
+        case ADD_POST_REQUEST: //отправка запроса на бэк
+            return {
+                ...store,  //это object destructuring
+                postResponse: {isLoaded: false, isFetching: true, isError: false, error: null} ///здесь отчищаются предыдущие значения и устанавливается что начинается загрузка
+            };
+        case ADD_POST_SUCCESS: //успешный ответ с бэка
+            return {
+                ...store,
+                postResponse: {isLoaded: true, isFetching: false, post: payload}, //записывается что загрузилось и закидывается свой пост в этот стейт
+                postsResponse: {
+                    ...store.postsResponse, //не изменяются предыдущие значения
+                    posts: [...store.posts, payload] //и через object destructuring добавляется новый объект в конец массива
+                }
+            };
+        case ADD_POST_FAILURE:
+            return {...store, postResponse: {isLoaded: false, isFetching: false, isError: true, error: payload}}; //добавление ошибки
+        default:
+            return store;
+    }
+}
+
+export default postReducer;*/
