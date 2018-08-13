fa3b3583cc6460afabfcb5f2d27e6e82f2ed3964
Torio Yoshiko <torio.yoshiko@gmail.com>
2018-08-13 18:17:04 +0300

    add some viewing changes

--- src/components/AllPost.js
+++ src/components/AllPost.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Post from './loadPosts'
 import getPosts from '../actions/posts'
-import './AllPost.css'
+import './css/AllPost.css'
 
 class AllPost extends Component {
   componentDidMount() {
--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Component } from 'react';
 import {connect} from 'react-redux';
-import './CreatePost.css';
+import './css/CreatePost.css';
 
 class CreatePost extends Component {
 
